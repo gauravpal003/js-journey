31d8c490bbe9d90bd8d178ce0f672a99eb299263
Gaurav <gauravpal2106@gmail.com>
2026-05-03 12:15:17 +0530

    data type

---
+++ js-journey-main/01_basic/02_dataType.js
@@ -0,0 +1,28 @@
+"Use strict " ; // treat all js code as newer version
+
+// alart (3+ 3) // we are useing node js not browser
+
+console.log(3 
+    +
+    3
+) // code readability should be high 
+
+console.log("Gaurav")
+
+ let name = "Gaurav1" // String
+ let age =25  //Number
+ let isLoggedIn =false // boolean 
+ let state ; 
+
+ // number => 2 to powe 53
+ // bigint 
+ //String => ""
+ //boolean => true / false
+ // null => standalone value 
+ // undefined
+ //symbol==> unique
+
+ // object 
+
+
+ console.log(typeof null)
